67b5544ba80a3456e9aae6a6dbe6a2541d932d22
Ning Wang <63277885+NingWang1999@users.noreply.github.com>
2025-02-08 22:37:26 +0800

    Update branch_registration.cpp

--- code/branch_registration.cpp
+++ code/branch_registration.cpp
@@ -1,18 +1,14 @@
 #include"branch_registration.h"
 
-//计算交点坐标
 pcl::PointXYZ intersectionOfTwoLines(const Eigen::Vector3d& line1Point, const Eigen::Vector3d& line1Vec,
 	const Eigen::Vector3d& line2Point, const Eigen::Vector3d& line2Vec) 
 {
-	// 检查两直线是否平行（即向量叉积为0）,不能直接norm，会消掉原有的正负号
 	double det = (line1Vec.cross(line2Vec))[2];
 	if (std::abs(det) < 1e-7) 
 	{
 		std::cerr << "Warning: The two lines are parallel or nearly parallel." << std::endl;
-		return pcl::PointXYZ{ NAN, NAN, 0 }; // 返回NaN表示无交点
+		return pcl::PointXYZ{ NAN, NAN, 0 };
 	}
-
-	// 计算交点坐标
 	double t = ((Eigen::Vector3d{ line2Point[0] - line1Point[0], line2Point[1] - line1Point[1], 0 }.cross(line2Vec))[2]) / det;
 	pcl::PointXYZ intersectionPoint = 
 	{
@@ -23,14 +19,14 @@ pcl::PointXYZ intersectionOfTwoLines(const Eigen::Vector3d& line1Point, const Ei
 
 	return intersectionPoint;
 }
-//配准
+//registration
 void BranchRegistration::alignCylinders_with_project(const std::pair<ClusterInfo, ClusterInfo> matched_clusters)
 {
 	std::cout << "\nStart branch registration!" << std::endl;
 	CylinderParams source_branch = matched_clusters.first.center_axis_radius;
 	CylinderParams target_branch = matched_clusters.second.center_axis_radius;
 
-	//将轴线向量和轴上一点都投影到XOY面
+	//progection XOY
 	Eigen::Vector3d p_source_axis(source_branch.axis[0], source_branch.axis[1], 0);
 	Eigen::Vector3d p_target_axis(target_branch.axis[0], target_branch.axis[1], 0);
 	p_source_axis.normalize();
@@ -38,39 +34,28 @@ void BranchRegistration::alignCylinders_with_project(const std::pair<ClusterInfo
 	Eigen::Vector3d p_source_point(source_branch.point[0], source_branch.point[1], 0);
 	Eigen::Vector3d p_target_point(target_branch.point[0], target_branch.point[1], 0);
 
-	Eigen::Matrix3d R = Eigen::Matrix3d::Identity();//旋转矩阵
-	Eigen::Vector3d t(0, 0, 0);//平移向量
+	Eigen::Matrix3d R = Eigen::Matrix3d::Identity();//rotation matrix
+	Eigen::Vector3d t(0, 0, 0);//translation vector
 
-	//求交点
 	pcl::PointXYZ cross_point = intersectionOfTwoLines(p_source_point, p_source_axis, p_target_point, p_target_axis);
-	Eigen::Vector3d rotation_cross_point(static_cast<double>(cross_point.x), static_cast<double>(cross_point.y), 0);//交点坐标的投影
+	Eigen::Vector3d rotation_cross_point(static_cast<double>(cross_point.x), static_cast<double>(cross_point.y), 0);
 
-	double dot_product = p_source_axis.dot(p_target_axis);//点乘结果用来求夹角,注意！这里的向量模是1
-	//double angle_rad = acos(std::abs(dot_product));//反余弦求夹角，结果约束为正
-	double angle_rad = acos(dot_product);//反余弦求夹角,0-180
+	double dot_product = p_source_axis.dot(p_target_axis);
+	double angle_rad = acos(dot_product);
 
-	//
-	// 
-	// 
-	//旋转轴，有正负
-	Eigen::Vector3d rotation_axis(0, 0, 1);//初始化旋转轴
+	Eigen::Vector3d rotation_axis(0, 0, 1);
 
 	rotation_axis = p_source_axis.cross(p_target_axis);
-	rotation_axis.normalize();//单位化
+	rotation_axis.normalize();
 
-	std::cout << "旋转轴：\n" << rotation_axis << std::endl;
+	std::cout << "rotation axis锛歕n" << rotation_axis << std::endl;
 
-	Eigen::AngleAxisd rotation_vector(angle_rad, rotation_axis);//得到轴角，单位是弧度
-	// 绕Z轴旋转的旋转矩阵
-	Eigen::Matrix3d rotZ = rotation_vector.matrix();//轴角转为旋转矩阵
-	//构建平移矩阵
+	Eigen::AngleAxisd rotation_vector(angle_rad, rotation_axis);
+	Eigen::Matrix3d rotZ = rotation_vector.matrix();
 	Eigen::Translation3d translation(-rotation_cross_point.x(), -rotation_cross_point.y(), 0);
 	Eigen::Translation3d inverseTranslation(rotation_cross_point.x(), rotation_cross_point.y(), 0);
 
-	// 构建整体变换矩阵：先平移到旋转中心，再绕Z轴旋转，最后平移回原来的位置
-	//是为了把原点移到交点处，所以平移是（-交点）
 	T = inverseTranslation * rotZ * translation;
-	// T = rotZ * translation;错误
 	std::cout << "Transform Matrix: \n" << T.matrix() << std::endl;
 
 }
@@ -81,9 +66,9 @@ void BranchRegistration::BranchCoarseRegistration()
 	*target_aligned_clouds = *target_clouds;
 
 	std::cerr << "BranchCoarseRegistration have finished!" << std::endl;
-	pcl::io::savePCDFileBinary("source_regis_clouds.pcd", *source_aligned_clouds);//保存结果点云
+	pcl::io::savePCDFileBinary("source_regis_clouds.pcd", *source_aligned_clouds);
 	std::cerr << "BranchCoarseRegistration have finished!" << std::endl;
-	pcl::io::savePCDFileBinary("target_regis_clouds.pcd", *target_aligned_clouds);//保存结果点云
+	pcl::io::savePCDFileBinary("target_regis_clouds.pcd", *target_aligned_clouds);
 
 }
 
