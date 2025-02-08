bb0bb42155c4983ee4e3f1c9a0d6573e3e746b43
Ning Wang <63277885+NingWang1999@users.noreply.github.com>
2025-02-08 22:48:05 +0800

    Update cylindrical_registration.cpp

--- code/cylindrical_registration.cpp
+++ code/cylindrical_registration.cpp
@@ -2,12 +2,12 @@
 
 void CylindricalRegistration::alignCylinders(const CylinderParams& src_cylinder, const CylinderParams& tgt_cylinder)
 {
-	Eigen::Matrix3d R = Eigen::Matrix3d::Identity();//旋转矩阵
-	Eigen::Vector3d t = Eigen::Vector3d::Identity();//平移向量
+	Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
+	Eigen::Vector3d t = Eigen::Vector3d::Identity();
 
 	t = tgt_cylinder.v_center - R * src_cylinder.v_center;
 
-	T = Eigen::Translation3d(t) * Eigen::Matrix3d(R);//先旋转后平移！左乘
+	T = Eigen::Translation3d(t) * Eigen::Matrix3d(R);
 
 	std::cout << "Transform Matrix: \n" << T.matrix() << std::endl;
 }
@@ -18,9 +18,9 @@ void CylindricalRegistration::TrunkCoarseRegistration()
 	pcl::transformPointCloud(*source_clouds, *source_aligned_clouds, T);
 
 	std::cerr << "TrunkCoarseRegistration have finished!" << std::endl;
-	pcl::io::savePCDFileBinary("source_aligned_clouds.pcd", *source_aligned_clouds);//保存结果点云
+	pcl::io::savePCDFileBinary("source_aligned_clouds.pcd", *source_aligned_clouds);
 	std::cerr << "TrunkCoarseRegistration have finished!" << std::endl;
-	pcl::io::savePCDFileBinary("target_aligned_clouds.pcd", *target_aligned_clouds);//保存结果点云
+	pcl::io::savePCDFileBinary("target_aligned_clouds.pcd", *target_aligned_clouds);
 }
 
 Eigen::Affine3d CylindricalRegistration::getMatix()
