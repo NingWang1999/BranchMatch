2a79506d8b1eafa3afc11a83e4bc64f1a566c197
Ning Wang <63277885+NingWang1999@users.noreply.github.com>
2025-02-08 22:48:54 +0800

    Update cylindrical_registration.h

--- code/cylindrical_registration.h
+++ code/cylindrical_registration.h
@@ -2,7 +2,6 @@
 #ifndef CYLINDRICAL_REGISTRATION_H
 #define CYLINDRICAL_REGISTRATION_H
 
-//#include "point_cloud_preprocessor.h"
 #include "cylinder_fitter.h"
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -11,7 +10,6 @@
 #include <cmath>
 #include <pcl/common/transforms.h>
 
-//圆柱的直接求旋转平移矩阵进行刚体变换
 class CylindricalRegistration {
 public:
     CylindricalRegistration(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud) :
@@ -21,19 +19,14 @@ public:
         target_aligned_clouds(new pcl::PointCloud<pcl::PointXYZ>),
         T(Eigen::Affine3d::Identity())
     {}
-    // 根据圆柱轴线求得旋转平移变换矩阵
     void alignCylinders(const CylinderParams& src_cylinder, const CylinderParams& tgt_cylinder);
-    //将source点云变换到target
     void TrunkCoarseRegistration();
-    //返回T
     Eigen::Affine3d getMatix();
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr getSourceAlignedCloud();
     pcl::PointCloud<pcl::PointXYZ>::Ptr getTargetAlignedCloud();
 
 private:
-    //PointCloudPreprocessor preprocessor();
-    //CylinderFitter source_fitter, target_fitter;
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_clouds, target_clouds;
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_aligned_clouds, target_aligned_clouds;
     Eigen::Affine3d T;
